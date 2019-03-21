de07da566bd9d973a0db3cc5c90a1b853013cbdf
Carlos <ca.orduz@uniandes.edu.co>
2019-03-20 21:41:53 -0500

    Casos de uso de listas

---
+++ src/app/vendedor/vendedor.ts
@@ -0,0 +1,20 @@
+/**
+* This class represents an editorial of the BookStore. 
+* It contains all the information relevant to the editorial.
+*/
+export interface Vendedor {
+    /**
+    * The editorial's id
+    */
+    id: number;
+
+    /**
+    * The editorial's name
+    */
+    name: string;
+
+    foto:string;
+  
+
+}
+
