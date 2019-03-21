09f55c0cd970ab38e178c96f8f3332c92ee70b07
Juan Pablo <jp.cano@uniandes.edu.co>
2019-03-21 07:46:38 -0500

    OnInit

--- src/app/Comprador/comprador.module.ts
+++ src/app/Comprador/comprador.module.ts
@@ -1,6 +1,5 @@
-import { NgModule } from '@angular/core';
+import { NgModule, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {OnInit} from '@angular/core'
 
 @NgModule({
     declarations: [],
