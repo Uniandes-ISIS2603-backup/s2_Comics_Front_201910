cd2c64883ea079e168cf243cb0b7927ae620b2fc
Sebastian Baquero <s.baqueroa@uniandes.edu.co>
2019-03-28 08:10:49 -0500

    Modulo comicDeseo

---
+++ src/app/ComicDeseo/ComicDeseo.ts
@@ -0,0 +1,22 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+ 
+ export class ComicDeseo{
+     
+     /**
+      * fecha de agregado del comic a la lista de deseos
+      */
+    // fechaAgregado:Date;
+      
+      nombre:String;
+      
+      id:Number;
+     
+     
+     
+ }
+
+
