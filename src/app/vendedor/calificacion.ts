6a54883b87e1daaa0a6ff4df48ad8ef1a1fa3f5a
Carlos <ca.orduz@uniandes.edu.co>
2019-03-31 18:59:24 -0500

    op

---
+++ src/app/vendedor/calificacion.ts
@@ -0,0 +1,20 @@
+/**
+* This class represents an editorial of the BookStore. 
+* It contains all the information relevant to the editorial.
+*/
+export interface Calificacion {
+    /**
+    * The editorial's id
+    */
+    id: number;
+
+    /**
+    * The editorial's name
+    */
+    puntuacion: number;
+
+    comentarios :string;
+  
+
+}
+
