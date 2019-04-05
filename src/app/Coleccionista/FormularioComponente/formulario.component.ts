475d5f767d452137720d13cfa4896c121171ee7c
Juan Pablo Cano <jp.cano@uniandes.edu.co>
2019-04-04 23:11:35 -0500

    Validación formulario casi acabada

--- src/app/Coleccionista/FormularioComponente/formulario.component.ts
+++ src/app/Coleccionista/FormularioComponente/formulario.component.ts
@@ -23,6 +23,22 @@ export class FormularioComponent implements OnInit
                 Validators.minLength(3),  
                 Validators.maxLength(30),  
                 Validators.pattern('^[a-zA-Z ]*$')
+            ]),
+            lastName: new FormControl('',[
+                Validators.required
+            ]),
+            alias: new FormControl('',[
+                Validators.required,
+                Validators.minLength(5),
+                Validators.maxLength(30)
+            ]),
+            email:new FormControl('', [
+                Validators.required,
+                Validators.pattern(/^([\w\-\.]+)@((\[([0-9]{1,3}\.){3}[0-9]{1,3}\])|(([\w\-]+\.)+)([a-zA-Z]{2,4}))$/)
+            ]),
+            password: new FormControl('',[
+                Validators.required,
+                Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{4,8}$/)
             ])
         });
     }
@@ -30,7 +46,8 @@ export class FormularioComponent implements OnInit
     onRegistrationFormSubmit():void
     {  
         this.isSubmitted = true;  
-        if(this.registrationForm.valid){        
+        if(this.registrationForm.valid)
+        {        
           console.log("User Registration Form Submit", this.registrationForm.value);  
         }
     }
