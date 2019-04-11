a25e7274a6b1ebc38c977068a64cb0b18d90c9d3
Sebastian Baquero <s.baqueroa@uniandes.edu.co>
2019-04-10 21:14:44 -0500

    mejoras

--- src/app/ComicDeseo/comicDeseo.service.ts
+++ src/app/ComicDeseo/comicDeseo.service.ts
@@ -11,9 +11,9 @@ import {Observable} from 'rxjs';
 import { ComicDeseoDetail } from './ComicDeseoDetail';
 import { environment } from '../../environments/environment';
 
-//const API_URL = "../../assets/";
-const API_URL = environment.apiURL;
-const comicsDeseo = '/comicDeseo';
+const API_URL = "../../assets/";
+//const API_URL = environment.apiURL;
+const comicsDeseo = '/ComicsDeseo.json';
 
 @Injectable()
 export class ComicDeseoService{
@@ -27,42 +27,36 @@ export class ComicDeseoService{
         
     }
 
+   
     /**
     * Returns the Observable object containing the editorial retrieved from the API
     * @returns The editorial
     */
-   getComicDeseoDetail(comicDeseoId): Observable<ComicDeseoDetail> {
-<<<<<<< HEAD
-    return this.http.get<ComicDeseoDetail>(API_URL +comicsDeseo+'/'+comicDeseoId);
-}
+  // getComicDeseoDetail(comicDeseoId): Observable<ComicDeseoDetail> {
+  //  return this.http.get<ComicDeseoDetail>(API_URL +comicsDeseo+'/'+comicDeseoId);
+//}
 
 /**
  * Obtiene el comprador a partir del alias
  * @param id Id del comicDeseo
  */
-    getComicDeseoById(id):Observable<ComicDeseoDetail>{
+   // getComicDeseoById(id):Observable<ComicDeseoDetail>{
 
-        return this.http.get<ComicDeseoDetail>(API_URL+comicsDeseo+'/'+id);
-    }
+    //    return this.http.get<ComicDeseoDetail>(API_URL+comicsDeseo+'/'+id);
+   // }
 
     /**
      * Crea un comic Deseo
      * @param comicDeseo el comic deseo que se va a crear
      */
 
-     createComicDeseo(comicDeseo):Observable<ComicDeseo>{
+    // createComicDeseo(comicDeseo):Observable<ComicDeseo>{
 
-        return this.http.post<ComicDeseo>(API_URL + comicsDeseo,comicDeseo)
-}           
+      //  return this.http.post<ComicDeseo>(API_URL + comicsDeseo,comicDeseo)
+//}           
      }
 
 
      
-=======
-    return this.http.get<ComicDeseoDetail>(API_URL +comicDeseoId);
-}
-
-}
->>>>>>> branchBaquero
 
 
