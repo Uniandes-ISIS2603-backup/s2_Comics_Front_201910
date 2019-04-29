293d5ed0ca143bdfb8710d897c38ef744ffea603
jenifer <jp.rodriguezv@uniandes.edu.co>
2019-04-29 08:23:38 -0500

    .

--- src/app/orden-pedido/create/create.component.ts
+++ src/app/orden-pedido/create/create.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Input,OnChanges ,ViewChild,ViewContainerRef} from '@angular/core';
 
 import {Vendedor} from '../../vendedor/vendedor';
-import { Comic } from 'src/app/comic/comic';
+import { Comic } from '../../Comic/Comic';
 
 @Component({
   selector: 'app-create',
