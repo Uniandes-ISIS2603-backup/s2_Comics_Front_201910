89db876b8b85870478f4de56618831230236e023
Carlos Orduz <ca.orduz@uniandes.edu.co>
2019-05-11 16:39:02 -0500

    Revert "Revert "Merge remote-tracking branch 'origin/master' into ramaOrduz""
    This reverts commit a1b9a9ed5143728754d4a2c1cef3d7a1dbd28900.

--- src/app/Coleccionista/LogInComponente/logIn.component.ts
+++ src/app/Coleccionista/LogInComponente/logIn.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Output } from "@angular/core";
-import { FormGroup, FormBuilder, FormControl, Validators } from "@angular/forms";
+import {FormGroup, FormBuilder, FormControl, Validators, FormArray} from "@angular/forms";
 import { CompradorService } from "../../Comprador/comprador.service";
 import { Comprador } from "../../Comprador/comprador";
 import { Router } from "@angular/router";
@@ -10,6 +10,8 @@ import {Expo} from "gsap/all";
 import $ from "jquery";
 import {AuthService} from "../../auth/auth.service";
 
+var close = 0;
+
 @Component({
     selector: 'app-login',
     templateUrl: 'logIn.component.html',
@@ -17,12 +19,38 @@ import {AuthService} from "../../auth/auth.service";
 })
 
 export class LogInComponent implements OnInit {
+    /**
+     *
+     */
     logInForm: FormGroup;
+
+    rolesGroup : FormGroup;
+
+    /**
+     *
+     */
     isSubmitted: boolean = false;
 
+    /**
+     *
+     */
     vendedor: Vendedor;
+
+    /**
+     *
+     */
     comprador: Comprador;
 
+    roles: Array<String> = ["Comprador", "Vendedor", "Administrador"];
+
+    /**
+     *
+     * @param formBuilder
+     * @param compradorService
+     * @param vendedorService
+     * @param router
+     * @param auth
+     */
     constructor(private formBuilder: FormBuilder,
                 private compradorService: CompradorService,
                 private vendedorService: VendedorService,
@@ -34,57 +62,142 @@ export class LogInComponent implements OnInit {
             ]),
             password: new FormControl('', [
                 Validators.required
+            ]),
+            role: new FormControl(null, [
+                Validators.required
             ])
         });
+
+        this.rolesGroup = this.formBuilder.group({
+            myValues: this.addValuesControl()
+        })
     }
-    //Golda Percival
-    onLogIngSubmit() {
+
+    /**
+     * Agrega los valores al grupo de control 'rolesGroup'
+     */
+    addValuesControl()
+    {
+        const arr = this.roles.map(object =>
+        {
+            return this.formBuilder.control(false);
+        });
+
+        return this.formBuilder.array(arr);
+    }
+
+    /**
+     * Funcion que se ejecuta cuando se da click al boton 'Sign In'
+     */
+    onLogInSubmit()
+    {
         this.isSubmitted = true;
 
-        if (this.logInForm.valid) {
+        //Valida que el formulario no tenga errores.
+        if (this.logInForm.valid)
+        {
             var alias: string = this.logInForm.get('alias').value;
             var password: string = this.logInForm.get('password').value;
+            var role: string = this.logInForm.get('role').value;
 
-            this.compradorService.getCompradorByAlias(alias).subscribe(cadena => {
-                if (cadena != null) {
-                    this.comprador = cadena;
-                    if (this.comprador.password == password) {
-                        this.router.navigate(['/comprador/' + this.comprador.id]);
-                        this.logInForm.reset();
+            if(role == "Comprador")
+            {
+                this.compradorService.getCompradorByAlias(alias).subscribe(cadena =>
+                {
+                    if(cadena != null)
+                    {
+                        this.comprador = cadena;
+                        if(this.comprador.password == password)
+                        {
+                            this.auth.logIn("Comprador", this.comprador.id);
+                            this.logInForm.reset();
+                        }
+                        else
+                        {
+                            alert("Error");
+                        }
+                    }
+                    else
+                    {
+                        alert("Error");
+                    }
+                });
+            }
+            else if(role == "Vendedor")
+            {
+                this.vendedorService.getVendedorByAlias(alias).subscribe(cadena =>
+                {
+                    if(cadena != null)
+                    {
+                        this.vendedor = cadena;
+                        if(this.vendedor.password == password)
+                        {
+                            this.auth.logIn(role, cadena.id);
+                            this.logInForm.reset();
+                        }
                     }
                     else {
-                        console.log("Error!");
+                        alert("Error");
                     }
-                }
-                else
-                {
-                    this.vendedorService.getVendedorByAlias(alias).subscribe( user =>
-                    {
-                        if(user != null)
-                        {
-                            this.vendedor = user;
-                            if(this.vendedor.password == password)
-                            {
-                                this.router.navigate(['/vendedores/' + this.vendedor.id]);
-                                this.logInForm.reset();
-                            }
-                            else
-                            {
-                                console.log("Error");
-                            }
-                        }
-                    });
-                }
-            });
-
+                });
+            }
+            else if(role == "Administrador")
+            {
+                this.auth.logIn(role, null);
+            }
         }
-        else {
-            this.logInForm.reset();
+        else
+        {
+            this.pop();
         }
     }
 
+    /**
+     * Funcion que me permite cerrar la ventana de dialogo de error
+     */
+    pop():void
+    {
+        if(close == 0)
+        {
+            $('.alert').css(
+                {
+                    'display':'block'
+                }
+            );
+            close = 1;
+        }
+        else if(close == 1)
+        {
+            $('.alert').css(
+                {
+                    'display':'none'
+                }
+            );
+            close = 0;
+        }
+    }
+
+    /**
+     * Funcion que me permite mostrar el password del usuario en el Login
+     */
+    showPassword():void
+    {
+        let password = $('#password'),
+            toggle = $('#show-password');
+
+        toggle.click(function()
+        {
+            toggle.is(':checked') ? password.attr('type', 'text'):password.attr('type', 'password');
+        })
+    }
+
+    /**
+     * La funcion del efecto mouse parallax el background, se utilizó
+     * TweenMax para su logro.
+     */
     background(): void
     {
+        document.body.style.overflow = 'hidden';
         var $layer_0 = $('.layer-0'),
             $layer_1 = $('.layer-1'),
             $layer_2 = $('.layer-2'),
@@ -96,17 +209,17 @@ export class LogInComponent implements OnInit {
         {
             var pos_x = event.pageX,
                 pos_y = event.pageY,
-                left = 0,
-                top = 0;
-            left = container_w / 2 - pos_x;
-            top  = container_h / 2 - pos_y;
+                left = container_w / 2 - pos_x,
+                top = container_h / 2 - pos_y;
+            // left = container_w / 2 - pos_x;
+            // top  = container_h / 2 - pos_y;
 
             TweenMax.to(
                 $layer_2,
                 1,
                 {
                     css: {
-                        transform: 'translateX(' + left / 12 + 'px) translateY(' + top/4 + 'px)'
+                        transform: 'translateX(' + left / 6 + 'px) translateY(' + top/4 + 'px)'
                     },
                     ease: Expo.easeOut,
                     overwrite: 'all'
@@ -117,7 +230,7 @@ export class LogInComponent implements OnInit {
                 1,
                 {
                     css: {
-                        transform: 'translateX(' + left / 4 + 'px)translateY(' + top/4 + 'px)'
+                        transform: 'translateX(' + left / 2 + 'px)translateY(' + top/4 + 'px)'
                     },
                     ease: Expo.easeOut,
                     overwrite: 'all'
@@ -128,7 +241,7 @@ export class LogInComponent implements OnInit {
                 10,
                 {
                     css: {
-                        transform: 'rotate(' + left / 200 + 'deg)'
+                        transform: 'rotate(' + left / 150 + 'deg)'
                     },
                     ease: Expo.easeOut,
                     overwrite: 'none'
@@ -136,7 +249,12 @@ export class LogInComponent implements OnInit {
         });
     }
 
-    ngOnInit() {
+    /**
+     * Funcion que se ejecuta al iniciar el componente
+     */
+    ngOnInit()
+    {
+        this.showPassword();
         this.background();
     }
 }
