8e099781e3272a96baa3bcea4f60c94cb3c4c73c
Juan Pablo Cano <jp.cano@uniandes.edu.co>
2019-05-21 01:58:29 -0500

    Cambios en los diseños

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import { AuthService } from './auth/auth.service';
+import {Router} from "@angular/router";
 
 /**
  * The app component. This component is the base of s2_comics-Front
@@ -9,6 +10,7 @@ import { AuthService } from './auth/auth.service';
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css']
 })
+
 export class AppComponent implements OnInit {
 
     /**
@@ -16,24 +18,49 @@ export class AppComponent implements OnInit {
      */
     title: String;
 
-    rol: String = localStorage.getItem("role");
+    rol: String = "";
    
-    id: String= localStorage.getItem("user");
-    /**
-     * Assigns a title to the web page
-     */
-    ngOnInit(): void {
-        this.title = "Tienda de Comics";
-        this.authService.start();
-       
-    }
+    id: number = 0;
 
     /**
      * @ignore
      */
-    constructor(private authService: AuthService) { }
+    constructor(private authService: AuthService,
+                private router: Router) { }
 
-    logout(): void {
+    /**
+     * Assigns a title to the web page
+     */
+    ngOnInit()
+    {
+        this.rol = localStorage.getItem("role");
+        this.id = +localStorage.getItem("user");
+        document.body.style.overflowY = 'show';
+        this.title = "Tienda de Comics";
+        this.authService.start();
+    }
+
+    /**
+     * Funcion que permite navegar hasta el perfil del comprador
+     */
+    compradorProfile()
+    {
+        this.router.navigateByUrl('/comprador/' + localStorage.getItem('user'));
+    }
+
+    /**
+     * Funcion que permite navegar hasta el carrito de compras.
+     */
+    carritoCompras()
+    {
+        this.router.navigateByUrl("/comprador/" + localStorage.getItem('user') + '/comics');
+    }
+
+    /**
+     * Funcion que permite cerrar la sesión activa.
+     */
+    logout(): void
+    {
         this.authService.logout()
     }
 }
